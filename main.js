0aa0d94de868336ee483870fddefe7963bc65792
kay <71146091+KayT98@users.noreply.github.com>
2023-06-01 23:33:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ let num = 0
 
 let herta = document.getElementById("kurugif")
 
-//add kurukuru sound when user click the image
+//add kurukuru sound when user click the image repeatedly
 function sound() {
-    let audio = document.getElementById("audio")
+    const audio = document.getElementById("audio")
+    const repeatAudio = audio.cloneNode(true) //this will make clones of the audio
+    document.body.appendChild(repeatAudio)
     audio.play()
 }
 
 function clickMe() {
-
     if (num <= 10)
     {
         num += 1
@@ -20,14 +21,25 @@ function clickMe() {
     //increase the speed
     let increaseSPD = document.getElementById("patSPD")
     
-    if (num >= 10 && num < 40) {
+    if (num >= 10 && num <= 50) {
         num += 3   
         increaseSPD.innerHTML = "SPD Boosted +3"
     }
 
-    if (num >= 40) 
+    if (num >= 50 && num <= 100) 
     {
         num += 6
         increaseSPD.innerHTML = "SPD Boosted +6"
-    }         
+    }    
+    
+    if (num >= 100 && num <= 1000) {
+        num += 100
+        increaseSPD.innerHTML = "SPD Boosted +100"
+    }
+
+    if (num == 1000) {
+        num += 10000
+        increaseSPD.innerHTML = "POWAAAAHHHH!!!"
+    }
 }
+
